ebe163e9136a5b7df20b2c52ea7e2a8857f25e49
Popololo0 <olivierpoire007@gmail.com>
2025-01-17 10:15:18 -0500

    Update vendredi
    Fuck le 3

--- devoir_2_section_1_POIO2730/devoi_2_section_1_POIO2730.py
+++ devoir_2_section_1_POIO2730/devoi_2_section_1_POIO2730.py
@@ -2,24 +2,32 @@
 
 # Exercice 1
 
-def factorial(nombre) :
+def factorial() :
+
+    nombre = int(input("Valeur nombre factoriel? "))
 
     Yes = nombre
 
     compteur = nombre
 
-    if 0 <= nombre < 256 and type(nombre) == int :
+    if 0 < nombre < 256 and type(nombre) == int :
         while compteur > 1 :
             compteur -= 1
             nombre = nombre * compteur
 
         print ("La réponse de " + str(Yes) + " factoriel est : " + str(nombre))
+
+    elif nombre == 0 :
+        nombre = 1
+        print("La réponse de " + str(Yes) + " factoriel est : " + str(nombre))
+
     else :
         print("Retourne lire le devoir...")
 
 #Exercice 2
 
-def liste_diviseur(n) :
+def liste_diviseur() :
+    n = int(input("Valeur nombre de la diviseur : "))
     i = 1
     liste = []
     if 0 < n < 2e16 and type(n) == int :
@@ -37,15 +45,101 @@ def liste_diviseur(n) :
 
     longueur = len(mot)
     i = 0
+    liste_mot = [mot]
     print(longueur)
     print(mot[i])
     while i < longueur :
        if mot[i] == mot[-i] :
-           i += 1'''
+           i += 1
+            print()'''
 
+#Exercice 4
+
+def convertisseur_temperature() :
+    i = 1
+    while i == 1 :
+        x = input("Que voulez-vous faire comme conversion? De C -> F : Faire C. De F -> C : Faire F. ")
+        if x == "C" :
+            temperature = (input("Quel est la température à convertire? "))
+            try :
+                temperature = float(temperature)
+            except :
+                convertisseur_temperature()
+                break
+            if temperature >= -270 :
+                temperature = (9 * temperature / 5) + 32
+                print("La température est de " + str(temperature) + " F.")
+                i += 1
+            else:
+                print("Ouin, non...")
+
+        elif x == "F" :
+            temperature = (input("Quel est la température à convertire? "))
+            try :
+                temperature = float(temperature)
+            except :
+                convertisseur_temperature()
+                break
+            if temperature >= -459.67:
+                temperature = (temperature - 32) * 5 / 9
+                print("La température est de " + str(temperature) + " C.")
+                i += 1
+            else:
+                print("Ouin, non...")
+
+        else :
+            print("Recommence!")
+
+#Exercice 5
+
+def coucher_a_cote_de_quelqun_est_radioactif() :
+    print("Get Duck!")
+    i = 1
+    while i == 1 :
+        x = input("Que voulez-vous faire comme conversion? De L/100Km -> MpG : Faire L. De MpG -> L/100Km : Faire M. ")
+        if x == "L" :
+            consommation = (input("Quel est la consommation à convertire? "))
+            try :
+                consommation = float(consommation)
+            except :
+                coucher_a_cote_de_quelqun_est_radioactif()
+                break
+            if consommation > 0 :
+                consommation = 235.215 * consommation
+                print("La consommation est de " + str(consommation) + " MpG.")
+                i += 1
+            else:
+                print("Ouin, non...")
+
+        elif x == "M" :
+            consommation = (input("Quel est la consommation à convertire? "))
+            try :
+                consommation = float(consommation)
+            except :
+                coucher_a_cote_de_quelqun_est_radioactif()
+                break
+            if consommation > 0:
+                consommation = consommation / 235.215
+                print("La consommation est de " + str(consommation) + " L/100km.")
+                i += 1
+            else:
+                print("Ouin, non...")
+
+        else :
+            print("Recommence!")
 #Exercice 1
-factorial(5)
+factorial()
+
 #Exercice 2
-liste_diviseur(50)
+liste_diviseur()
+
 #Exercice 3
-#palindrome("oui")
+#palindrome("ete")
+
+#Exercice 4
+convertisseur_temperature()
+
+#Exercice 5
+coucher_a_cote_de_quelqun_est_radioactif()
+
+#Exercice 6
