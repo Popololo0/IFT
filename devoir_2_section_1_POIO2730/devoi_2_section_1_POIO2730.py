adaa46d90a805367e414cc0dfce37ffbb9c0c582
Popololo0 <olivierpoire007@gmail.com>
2025-01-19 09:47:42 -0500

    Il ne manque plus que les commentaires
    Genre 10 min max

--- devoir_2_section_1_POIO2730/devoi_2_section_1_POIO2730.py
+++ devoir_2_section_1_POIO2730/devoi_2_section_1_POIO2730.py
@@ -1,31 +1,25 @@
-#Voici la section 1 du devoir 2 d'IFT 211 d'Olivier Poiré et d'Alexandre Guay. La date de remise est le 22 janvier 2025.
+#Voici la section 1 du devoir 2 d'IFT 211 d'Olivier Poiré et d'Alexandre Guay. La date de remise est oui.
 
 # Exercice 1
-def factorial() :
 
-    nombre = int(input("Valeur nombre factoriel? "))
+def factorial(nombre) :
 
-    Yes = nombre
+    affichage = nombre
 
     compteur = nombre
 
-    if 0 < nombre < 256 and type(nombre) == int :
+    if 0 <= nombre < 256 and type(nombre) == int :
         while compteur > 1 :
             compteur -= 1
             nombre = nombre * compteur
 
-        print ("La réponse de " + str(Yes) + " factoriel est : " + str(nombre))
-
-    elif nombre == 0 :
-        nombre = 1
-        print("La réponse de " + str(Yes) + " factoriel est : " + str(nombre))
-
+        print ("La réponse de " + str(affichage) + " factoriel est : " + str(nombre))
     else :
         print("Retourne lire le devoir...")
 
 #Exercice 2
-def liste_diviseur() :
-    n = int(input("Valeur nombre de la diviseur : "))
+
+def liste_diviseur(n) :
     i = 1
     liste = []
     if 0 < n < 2e16 and type(n) == int :
@@ -38,23 +32,30 @@ def liste_diviseur() :
         print("Retourne lire le devoir...")
 
 #Exercice 3
-'''def palindrome(mot) :
 
-    longueur = len(mot)
+def palindrome(mot) :
+
+    if mot != str(mot) or len(mot) >= 100 :
+        print("Franchement...")
+        return 0
+
+    liste_mot = mot.lower()
+
+    liste_mot = list(liste_mot)
+
+    tom = list(reversed(mot))
     i = 0
-    liste_mot = [mot]
-    print(longueur)
-    print(mot[i])
-    while i < longueur :
-       if mot[i] == mot[-i] :
-           i += 1
-            print()'''
+    while i < len(mot) // 2 :
+        if liste_mot[i] != tom[i] :
+            print(mot + " n'est pas un palindrome!")
+            return 0
+        i += 1
+    print(mot + " est un palindrome!")
+    return 1
 
 #Exercice 1
 factorial()
-
 #Exercice 2
 liste_diviseur()
-
 #Exercice 3
-#palindrome("ete")
+palindrome()
