cadd529338c25f2c82d738b0a54e1bc6972958be
Popololo0 <olivierpoire007@gmail.com>
2025-02-12 10:05:39 -0500

    période 1
    good

--- Devoir_final_Invo/main.py
+++ Devoir_final_Invo/main.py
@@ -2,16 +2,39 @@ def lecture_nettoyage (chemin) :
     texte = open(chemin, 'r')
     texte = texte.read()
     indesirable = ["'", "...", ">", "<", "~", ",", ".", ":", ";", "!", "?", "(", ")", "-", "[", "]", "{", "}", "#", "$", "%", "&", "*", "+", "/", "=", "|", "_"]
-    for x in range(0, len(indesirable)):
+    for x in range(0, len(indesirable)) :
         texte = texte.replace(indesirable[x], " ")
-        #texte = texte.strip() # Wtf que ça ne fonctionnne pas...
-        texte_net = texte.lower()
 
-    # Vérifier if len(mot) < 2 : strip (mot)
-    print(texte_net)
+    texte = texte.lower()
+    mot = texte.split()
+    liste = []
+    for x in range(0, len(mot)) :
+        if len(mot[x]) > 2 :
+            liste.append(mot[x])
+    texte = " ".join(liste)
 
-def analyse_texte (texte_net) :
-    lettre = len(texte_net)
+    return texte
+
+def analyse_texte(texte_net) :
+
+    liste_lettre = []
+    liste_mot = []
+
+    nope = texte_net.split()
+    len_lettre = len(texte_net)
+    len_mot = len(nope)
 
 
-lecture_nettoyage("sherlock_holmes.txt")
+    for x in range(0, len_lettre) :
+        if texte_net[x] not in liste_lettre :
+            liste_lettre.append(texte_net[x])
+
+    '''for x in range(0, len_mot) :
+        if nope[x] not in liste_mot :
+            liste_mot.append(nope[x])'''
+
+    return print((len_lettre, len_mot, len(liste_lettre), len(liste_mot)))
+
+
+texte_net = lecture_nettoyage("sherlock_holmes.txt")
+analyse_texte(texte_net)
