43c6730fce049503177db928db045f4290d1d16a
Popololo0 <olivierpoire007@gmail.com>
2025-02-12 20:13:24 -0500

    Compil
    Yeh Haw

--- Devoir_final_Invo/main.py
+++ Devoir_final_Invo/main.py
@@ -1,7 +1,22 @@
+import unicodedata
+
+def supprimer_accents(texte_net):
+    """
+    Supprime les accents d'une chaîne de caractères.
+    Fonction utilitaire pour le devoir 5.
+    """
+    forme_nfkd = unicodedata.normalize('NFKD', texte_net)
+    uniquement_ascii = forme_nfkd.encode('ASCII', 'ignore')
+    uniquement_utf8 = uniquement_ascii.decode("utf-8")
+    return str(uniquement_utf8)
+
 def lecture_nettoyage (chemin) :
+
     texte = open(chemin, 'r')
     texte = texte.read()
-    indesirable = ["'", "...", ">", "<", "~", ",", ".", ":", ";", "!", "?", "(", ")", "-", "[", "]", "{", "}", "#", "$", "%", "&", "*", "+", "/", "=", "|", "_"]
+
+    indesirable = ["'", "...", ">", "<", "~", ",", ".", ":", ";", "!", "?", "(", ")", "-", "[", "]", "{", "}", "#", "$", "%", "&", "*", "+", "/", "=", "|", "_", "@", "^", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
     for x in range(0, len(indesirable)) :
         texte = texte.replace(indesirable[x], " ")
 
@@ -11,8 +26,9 @@ def lecture_nettoyage (chemin) :
     for x in range(0, len(mot)) :
         if len(mot[x]) > 2 :
             liste.append(mot[x])
-    texte = " ".join(liste)
 
+    texte = " ".join(liste)
+    texte = supprimer_accents(texte)
     return texte
 
 def analyse_texte(texte_net) :
@@ -28,6 +44,7 @@ def analyse_texte(texte_net) :
     for x in range(0, len_lettre) :
         if texte_net[x] not in liste_lettre :
             liste_lettre.append(texte_net[x])
+    liste_lettre.remove(" ")
 
     for x in range(0, len_mot) :
         if nope[x] not in liste_mot :
@@ -35,6 +52,19 @@ def analyse_texte(texte_net) :
 
     return print((len_lettre, len_mot, len(liste_lettre), len(liste_mot)))
 
+def inversoin_texte(texte_net) :
+
+    o = texte_net[::-1]
+
+    x = str(texte_net)
+    h = x.split()
+    j = h[::-1]
+    k = " ".join(j)
+
+    return o, k
 
 texte_net = lecture_nettoyage("sherlock_holmes.txt")
+
 analyse_texte(texte_net)
+
+inversoin_texte(texte_net)
